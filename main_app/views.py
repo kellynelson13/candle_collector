f383bdbf282038ba59ecece4ae0764ae5049ff4b
Kelly Nelson <kelnel86@gmail.com>
2022-10-17 12:58:41 -0500

    initial styling and candle seed data to make index page

--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,22 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+class Candle:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, scent, color, ounces, burn_time):
+    self.scent = scent
+    self.color = color
+    self.ounces = ounces
+    self.burn_time = burn_time
+  
+    
+
+candles = [
+  Candle('Tea Tree and Spearmint', 'cream', '5oz', '5 hours'),
+  Candle('Bridesmaid', 'white', '3oz', '4 hours'),
+  Candle('Honeycrisp Apple', 'green', '5.7oz', '6 hours')
+]
+
+
+def candles_index(request):
+  return render(request, 'candles/index.html', { 'candles': candles})
